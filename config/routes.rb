7605e84c16148bbd375300f3df1320fa2121a14e
Jody Vanden <jody.vanden@gmail.com>
2017-03-10 14:15:51 +0100

    install figaro

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses
   end
-
+  root 'cocktails#index'
   resources :doses, only: [ :destroy ]
 
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
